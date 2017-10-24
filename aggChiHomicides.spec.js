79735492d0c828aa3cba74440e2e775d017df7ea
Willy Raedy <wraedy@gmail.com>
2017-10-24 16:31:19 -0500

    creates chicago crime data aggregator function and tests

---
+++ aggChiHomicides.spec.js
@@ -0,0 +1,38 @@
+/* global expect describe test */
+
+const fs = require('fs');
+
+const getChiHomicideData = require('./getChiHomicideData');
+
+describe('Get Chicago Homicides', () => {
+  test('returns an object', () => {
+    getChiHomicideData('./willys_corner/testCrimeData.json')
+      .then(output => {
+        expect(typeof output).toBe('object');
+      })
+      .catch(err => {
+        console.error(err);
+        throw new Error('getChiHomicideData rejected unexpectedly');
+      });
+  });
+  test('aggregates chicago homicides by community area', (done) => {
+    let results;
+    const readable = fs.createReadStream('./willys_corner/testCrimeData.json');
+    readable.on('data', chunk => {
+      const string = chunk.toString();
+      results = JSON.parse(string).results;
+    });
+    readable.on('close', () => {
+      getChiHomicideData('./willys_corner/testCrimeData.json')
+        .then(output => {
+          expect(typeof output).toBe('object');
+          expect(output).toEqual(results);
+          done()
+        })
+        .catch(err => {
+          console.error(err);
+          throw new Error('getChiHomicideData rejected unexpectedly');
+        });
+    });
+  });
+});
