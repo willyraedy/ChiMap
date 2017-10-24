e5808ecbb2702b79342a08f6af50bbe8f35de799
Willy Raedy <wraedy@gmail.com>
2017-10-24 17:42:14 -0500

    refactor file structure

--- dataUtils/createChiData.js
+++
@@ -1,8 +0,0 @@
-const getChiHomicideData = require('./getChiHomicideData');
-const convertTo100k = require('./convertTo100k');
-
-const crimeData = getChiHomicideData('../willys_corner/chiCrimeData.json');
-const popData =
-
-
-
