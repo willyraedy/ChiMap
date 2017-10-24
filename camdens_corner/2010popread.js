f08ecc72019b01b6dcf29ba4ed6c1e5c1ffca8ca
Camden Ko <camdenko@gmail.com>
2017-10-24 17:05:11 -0500

    removed camdens corner

--- camdens_corner/2010popread.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs')
-fs.readFile('./2010pop.txt', (err, data) => {
-  const proccessed = data
-    .toString('utf16le')
-    .split('\r')
-    .map(row => row.trim().split('\t'))
-    .reduce((accum, row) => Object.assign(accum, { [row[1]]: { name: row[0], population: +row[2] } }), {})
-  fs.writeFile('2010pop.json', JSON.stringify(proccessed, null, '\t'), () => console.log('done'))
-})
